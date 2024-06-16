bc9cce651783d35056f1492017faf3734a841a2a
Tyler Cook <10459406+cilki@users.noreply.github.com>
2024-06-16 18:06:59 -0500

    wip: add initial CI

--- outpost/src/cloudflare.rs
+++ outpost/src/cloudflare.rs
@@ -1,3 +1,60 @@
-pub fn start() {
-    let cloudflared_binary = include_bytes!(env!("CLOUDFLARED_PATH"));
+use anyhow::Result;
+use serde::Serialize;
+use std::process::{Child, Command};
+use tracing::instrument;
+
+#[derive(Serialize)]
+pub struct CloudflareConfigIngress {
+    pub hostname: String,
+    pub service: String,
+}
+
+#[derive(Serialize)]
+pub struct CloudflareConfig {
+    pub tunnel: String,
+    #[serde(rename = "credentials-file")]
+    pub credentials_file: String,
+
+    pub ingress: Vec<CloudflareConfigIngress>,
+}
+
+#[derive(Debug)]
+pub struct CloudflareProxy {
+    /// The cloudflared child process which actually handles the routing
+    pub process: Child,
+}
+
+impl CloudflareProxy {
+    #[instrument(ret)]
+    pub fn new() -> Result<Self> {
+        let temp = tempfile::TempDir::new()?;
+
+        // Generate config
+        let config = CloudflareConfig {
+            tunnel: "generated".into(),
+            credentials_file: temp
+                .path()
+                .join("tunnel.json")
+                .to_string_lossy()
+                .to_string(),
+            ingress: vec![],
+        };
+
+        // Write config
+        let config_path = temp.path().join("config.yml");
+        std::fs::write(&config_path, serde_yaml::to_string(&config)?)?;
+
+        Ok(Self {
+            process: Command::new("cloudflared")
+                .arg("tunnel")
+                .arg("run")
+                .arg("--config")
+                .arg(config_path.to_string_lossy().to_string())
+                // Try to not cede any more control to cloudflare
+                .arg("--no-autoupdate")
+                .arg("--management-diagnostics")
+                .arg("false")
+                .spawn()?,
+        })
+    }
 }
--- outpost/src/main.rs
+++ outpost/src/main.rs
@@ -1,6 +1,27 @@
+use anyhow::Result;
+use axum::{routing::get, Router};
+use clap::Parser;
+use std::process::ExitCode;
+use tokio::net::TcpListener;
+
 #[cfg(feature = "cloudflare")]
 pub mod cloudflare;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct CommandLine {}
+
+#[tokio::main]
+async fn main() -> Result<ExitCode> {
+    let args = CommandLine::parse();
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+
+    let app = Router::new();
+    // .route("/", get(crate::api::index));
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+    Ok(ExitCode::SUCCESS)
 }
